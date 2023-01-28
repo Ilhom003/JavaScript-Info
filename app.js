fc84be0654af929a4e0cb5f62ad681a0337f99c6
ilhom003 <abdulahatov03gmail.com>
2023-01-28 04:52:01 +0300

    type-conversion

---
+++ app.js
@@ -0,0 +1,64 @@
+// let a = 1;
+// let b = '1';
+// console.log(a+b);
+
+// let a = '10'
+// let b = '10.3'
+
+// console.log(Number(b));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseFloat(b));
+
+// console.log(false == 0);
+// console.log(true == 1);
+
+
+// let a = 11;
+// // a++
+// // console.log(a++);
+
+// console.log(a++); //11
+// console.log(a);//12
+// console.log(++a);//13
+// console.log(a); //13
+// console.log(a++);//13
+// console.log(a++);//14
+
+// let a = 1000;
+// a += 10
+// console.log(a);
+
+
+
+// H
+// Homework
+// 1
+// let a = 1, b = 1;
+// let c= ++a;
+// let d= b++;
+// console.log(c,d);
+
+// 2
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x);
+
+// 3
+// console.log('' + 1 + 0);
+// console.log('' - 1 + 0);
+// console.log(true + false);
+// console.log(6/'3');
+// console.log('2'*'3');
+// console.log(4+5+'px');
+// console.log('$' + 4 + 5);
+// console.log('-9'+5);
+// console.log('-9'-5);
+// console.log(null+1);
+// console.log(undefined +1);
+// console.log('\t\n '-2);
+
+
+// console.log(null === undefined);
+
+// console.log(typeof undefined);
+// console.log(true === 'dasda');
